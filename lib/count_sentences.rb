9034bfd652a287b5b87df7076f255e184dcd2774
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-11-01 13:46:54 +0300

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
+  attr_accessor :string
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).count{|sentence| sentence.length > 0}
   end
 end
